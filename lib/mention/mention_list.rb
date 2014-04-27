0c32a74f26a86cc0c3094d891066349721c44f95
Synoptase <synoptase@gmail.com>
2014-04-27 17:44:26 +0200

    Additional attributes for paging purposes

--- lib/mention/mention_list.rb
+++ lib/mention/mention_list.rb
@@ -3,6 +3,8 @@ module Mention
     include Virtus.value_object(strict: true)
     values do
       attribute :mentions, Array[Mention]
+      attribute :_links
+      attribute :reached_history_limit, Boolean
     end
 
     extend Forwardable
